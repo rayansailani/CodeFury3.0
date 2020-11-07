a4b3dfaf378a9644ae3cbe2dbd31734fb7eb12c0
unknown <rayanbackup1@gmail.com>
2020-11-07 11:56:47 +0530

    changed file structure

--- codefury/settings.py
+++ codefury/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # employment details app
+    'employment',
+    # accounts app
+    'accounts_app',
 ]
 
 MIDDLEWARE = [
